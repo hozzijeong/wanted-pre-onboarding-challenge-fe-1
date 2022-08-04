c0d1b47b66fe912675a3ec711a2e0ee80b14d932
hozzijeong <ghwls0905@naver.com>
2022-08-04 15:50:32 +0900

    Feat: Login input 적용

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,8 +1,29 @@
+import React, { useState } from "react";
+
 function Login() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const changeValue = (
+    e: React.FormEvent<HTMLInputElement>,
+    fnc: React.Dispatch<React.SetStateAction<string>>,
+  ) => {
+    const { value } = e.currentTarget;
+    fnc(value);
+  };
+
   return (
     <form method="post">
-      <input type="email" />
-      <input type="password" />
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => changeValue(e, setEmail)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => changeValue(e, setPassword)}
+      />
       <input type="submit" value="로그인" />
     </form>
   );
