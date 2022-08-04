304abfcd89092ed99286a172d7dca6413f88c5fe
hozzijeong <ghwls0905@naver.com>
2022-08-04 15:21:28 +0900

    Feat: route 파일 생성 및 path 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import URLRoutes from "./routes/Route";
 
 function App() {
-  return <div></div>;
+  return <URLRoutes />;
 }
 
 export default App;
--- src/routes/Route.tsx
+++ src/routes/Route.tsx
@@ -1,3 +1,17 @@
-function URLRoutes() {}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function URLRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/todo">
+          <Route path="/todo/:id" />
+        </Route>
+        <Route path="/auth/login" />
+        <Route path="/auth/auth" />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default URLRoutes;
