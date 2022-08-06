d69cd1449cb23a4e8aee879bb2525f605a35f8da
hozzijeong <ghwls0905@naver.com>
2022-08-06 21:15:41 +0900

    Refactor: CreateTodo 컴포넌트 분리

---
+++ src/components/CreateTodo.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { useSetRecoilState } from "recoil";
+import { createTodos, getTodosAPI } from "../api/apis";
+import { todosAtom } from "../atom";
+import { inputChangeHandler } from "../utility/handler";
+import { ITodos } from "../utility/types";
+
+interface ICreateTodo {
+  token: string | null;
+}
+
+function CreateTodo({ token }: ICreateTodo) {
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+
+  const setTodos = useSetRecoilState<ITodos[]>(todosAtom);
+
+  const onSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    if (typeof token === "string") {
+      createTodos({ title, content }, token).then((data) => {
+        if (data?.details) {
+          alert(data.details);
+        } else {
+          setTitle("");
+          setContent("");
+          getTodosAPI(token).then((data) => setTodos(data.data));
+        }
+      });
+    }
+  };
+  return (
+    <div>
+      <h2>Add Task</h2>
+
+      <input
+        style={{ width: "98%" }}
+        type="text"
+        placeholder="type title"
+        value={title}
+        onChange={(e) => inputChangeHandler({ e, fnc: setTitle })}
+      />
+
+      <textarea
+        style={{ height: "100%", width: "98%" }}
+        value={content}
+        onChange={(e) => inputChangeHandler({ e, fnc: setContent })}
+      ></textarea>
+
+      <button onClick={(e) => onSubmit(e)}>추가하기</button>
+    </div>
+  );
+}
+
+export default CreateTodo;
--- src/routes/Todo.tsx
+++ src/routes/Todo.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import { createTodos, getTodosAPI } from "../api/apis";
 import { todosAtom } from "../atom";
+import CreateTodo from "../components/CreateTodo";
 import TodoItem from "../components/TodoItem";
 import { inputChangeHandler } from "../utility/handler";
 import { ITodos } from "../utility/types";
@@ -12,8 +13,6 @@ function Todos() {
   const navigation = useNavigate();
   const [token, setToken] = useState(localStorage.getItem("token"));
   const [todos, setTodos] = useRecoilState<ITodos[]>(todosAtom);
-  const [title, setTitle] = useState<string>("");
-  const [content, setContent] = useState<string>("");
 
   useEffect(() => {
     if (!token) navigation("/auth/login");
@@ -22,21 +21,6 @@ function Todos() {
         .then((data) => setTodos(data.data))
         .catch((e) => console.log(e));
   }, [token]);
-
-  const onSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    e.preventDefault();
-    if (typeof token === "string") {
-      createTodos({ title, content }, token).then((data) => {
-        if (data?.details) {
-          alert(data.details);
-        } else {
-          setTitle("");
-          setContent("");
-          getTodosAPI(token).then((data) => setTodos(data.data));
-        }
-      });
-    }
-  };
 
   const logoutHandler = () => {
     localStorage.removeItem("token");
@@ -47,25 +31,7 @@ function Todos() {
     <div>
       <h1>Tasks</h1>
       <button onClick={logoutHandler}>로그아웃</button>
-      <div>
-        <h2>Add Task</h2>
-
-        <input
-          style={{ width: "98%" }}
-          type="text"
-          placeholder="type title"
-          value={title}
-          onChange={(e) => inputChangeHandler({ e, fnc: setTitle })}
-        />
-
-        <textarea
-          style={{ height: "100%", width: "98%" }}
-          value={content}
-          onChange={(e) => inputChangeHandler({ e, fnc: setContent })}
-        ></textarea>
-
-        <button onClick={(e) => onSubmit(e)}>추가하기</button>
-      </div>
+      <CreateTodo token={token} />
       <hr />
       <div>
         <ul>{[...todos.map((x: ITodos) => <TodoItem key={x.id} {...x} />)]}</ul>
